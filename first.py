9eb6707e1552a034902192ca9aa7762ba6d83b12
unknown <footbolistvova@mail.ru>
2019-12-27 11:39:07 +0300

    new

--- first.py
+++ first.py
@@ -25,6 +25,24 @@ for event in vk_api.longpoll.VkLongPoll(vk_session).listen():
     if event.type == vk_api.longpoll.VkEventType.MESSAGE_NEW and event.to_me:
         text = event.text.lower()
         print(event.user_id)
-        vk.messages.send(user_id=event.user_id,
-                          random_id=random.randint(1, 10 ** 9),
-                          message='Привет!', keyboard=create_keyb1(['Здарова!', 'Hi!']))
+        bred = True
+        users = {}
+        if event.user_id not in users:
+            users[event.user_id] = 0
+        if text == 'начать' or text == 'help':
+            bred = False
+            vk.messages.send(user_id=event.user_id,
+                             random_id=random.randint(1, 10 ** 9),
+                             message='Привет! Я чат-бот дня самоуправления. Если ты еще не зарегистрирровался,' +
+                                     'то скорее нажимай на кнопку и присоединяйся к нам.',
+                             keyboard=create_keyb1(['Регистрация', 'Расписание', 'help']))
+        elif text == 'расписание':
+            vk.messages.send(user_id=event.user_id,
+                             random_id=random.randint(1, 10 ** 9),
+                             message='расписание',
+                             keyboard=create_keyb1(['Регистрация', 'Расписание', 'help']))
+        if bred:
+            vk.messages.send(user_id=event.user_id,
+                             random_id=random.randint(1, 10 ** 9),
+                             message='Извините, я Вас не понимаю(',
+                             keyboard=create_keyb1(['Регистрация', 'Расписание', 'help']))
